02d72edb174b186d2fa637e93f3c256e3806eca6
Rafael <rafael.sovza@gmail.com>
2022-10-09 20:07:45 -0300

    user, role

---
+++ src/main/java/com/atlas/dslearn/entities/Role.java
@@ -0,0 +1,24 @@
+package com.atlas.dslearn.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+}
